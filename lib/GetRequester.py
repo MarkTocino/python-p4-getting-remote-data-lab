2fc49ddd2dc7c251bce8de17c702060f414aeaf3
MarkTocino <Mark_tocino@yahoo.com>
2023-08-15 16:33:49 -0400

    Completed Lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        list = []
+        # here my_request is a json dictionary and we ran the method def_response_body for the response and we have the response content and now the response is a dict. then we appended it to an empty list
+        my_requests = json.loads(self.get_response_body())
+        for my_request in my_requests:
+            list.append(my_request)
+        return list
